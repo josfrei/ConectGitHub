90b6f77f3805ebd05b40d72c47c600d70093666a
josef <josef@LAPTOP-22O96BBN>
2023-10-17 12:31:00 +0200

    Primera Conexión

---
+++ src/main/java/com/mycompany/conectgithub/ConectGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conectgithub;
+
+/**
+ *
+ * @author josef
+ */
+public class ConectGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Moncho!");
+    }
+}
